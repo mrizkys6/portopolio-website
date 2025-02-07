1f761dc43dc314070042dfb0f3207c46d7d523ee
MRizkyS <154035469+mrizkys6@users.noreply.github.com>
2025-02-07 23:11:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: ['index.html'],
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#6d28d9',
+                dark: '#0f172a',
+                abang: '#FF0068',
+            },
+            screens: {
+                '2xl': '1320px',
+            },
+        },
+    },
+    plugins: [],
+};
